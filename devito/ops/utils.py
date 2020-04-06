4f2f8cc2834eb79ed19465f962b690a26511bac5
Gabriel Sebastian von Conta <mrgabral@gmail.com>
2020-04-06 16:20:41 -0300

    adding previous fix to division by pointer resulting in commented code

--- devito/ops/utils.py
+++ devito/ops/utils.py
@@ -9,7 +9,7 @@ from devito.symbolics import Literal, Macro
 namespace = OrderedDict()
 AccessibleInfo = namedtuple(
     'AccessibleInfo',
-    ['accessible', 'time', 'shift', 'origin_name'])
+    ['accessible', 'safe_accessible', 'time', 'shift', 'origin_name'])
 
 OpsDatDecl = namedtuple(
     'OpsDatDecl',
